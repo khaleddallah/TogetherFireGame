51aefdb428c5fb2bab796ca51bc0dfb6d9ce86ed
khaled0 <kahled.dallah0@gmail.com>
2021-07-03 01:08:00 +0300

    change ep time| change gold icons | change grid colors

--- lose.sh
+++ lose.sh
@@ -1,9 +1,9 @@
 # reg="http://fa3b8a8136cd.ngrok.io/lose"
 
-reg="http://0.0.0.0:5000/lose"
+reg="http://59c5c8152da2.ngrok.io/lose"
 
 
-# curl   --header 'Content-Type: application/json'  --request POST   --data '{"name":"dddd"}'  http://0.0.0.0:5000/reg
+curl   --header 'Content-Type: application/json'  --request POST   --data '{"pindex":"0"}'s  $regs
 curl   --header 'Content-Type: application/json'  --request POST   --data '{"pindex":"1"}'  $reg
-# curl   --header 'Content-Type: application/json'  --request POST   --data '{"pindex":"2"}'  $reg
-# curl   --header 'Content-Type: application/json'  --request POST   --data '{"pindex":"3"}'  $reg
+curl   --header 'Content-Type: application/json'  --request POST   --data '{"pindex":"2"}'  $reg
+curl   --header 'Content-Type: application/json'  --request POST   --data '{"pindex":"3"}'  $reg
